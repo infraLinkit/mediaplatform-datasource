4d5268bac12398266bc948932b05b03555eaa90f
Wilie Wahyu Hidayat <wilie.hidayat87@gmail.com>
2024-11-16 17:00:02 +0700

    add redis counter-2

--- entity/rediscounter.go
+++ entity/rediscounter.go
@@ -3,5 +3,6 @@ package entity
 type RedisCounter struct {
 	Name       string `json:"name"`
 	Date       string `json:"date"`
+	Time       string `json:"time"`
 	HttpStatus int    `json:"http_status"`
 }

92904ad5078c4383f384706596d2fd4f3207dffd
Wilie Wahyu Hidayat <wilie.hidayat87@gmail.com>
2024-11-16 14:10:41 +0700

    config bring to datasource

--- entity/config.go
+++ entity/config.go
@@ -2,11 +2,28 @@ package entity
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
+	"strconv"
 	"time"
 
+	"github.com/go-redis/redis"
 	"github.com/gofiber/storage/rueidis"
+	"github.com/infraLinkit/mediaplatform-datasource/helper"
 	"github.com/sirupsen/logrus"
 	"github.com/wiliehidayat87/rmqp"
+)
+
+const (
+	//APP_PATH       = "/app"
+	APP_PATH       = "/Users/wiliewahyuhidayat/Documents/GO/mediaplatform/cores/lp" // local
+	EVENT_TRAFFIC  = "Traffic"
+	EVENT_LANDING  = "Landing"
+	EVENT_CLICK    = "Click Landing"
+	EVENT_REDIRECT = "Redirect"
+
+	OK_DESC          = "OK"
+	BAD_REQUEST_DESC = "Bad Request"
 )
 
 type (
@@ -42,3 +59,123 @@ type (
 		Rmqp   rmqp.AMQP
 	}
 )
+
+func InitCfg() *Cfg {
+
+	loc, _ := time.LoadLocation(os.Getenv("TZ"))
+
+	rabbitmq_port, _ := strconv.Atoi(os.Getenv("RABBITMQPORT"))
+	redis_port, _ := strconv.Atoi(os.Getenv("REDISPORT"))
+	redis_exp, _ := strconv.Atoi(os.Getenv("REDISKEYEXPIRE"))
+
+	cfg := &Cfg{
+		AppHost:              os.Getenv("APPHOST"),
+		RedisHost:            os.Getenv("REDISHOST"),
+		RedisPort:            redis_port,
+		RedisPwd:             os.Getenv("REDISPASSWORD"),
+		RedisKeyExpiration:   int64(redis_exp),
+		PSQLHost:             os.Getenv("DB_HOST"),
+		PSQLUsername:         os.Getenv("DB_USERNAME"),
+		PSQLPassword:         os.Getenv("DB_PASSWORD"),
+		PSQLPort:             os.Getenv("DB_PORT"),
+		PSQLDB:               os.Getenv("DB_DATABASE"),
+		RabbitMQHost:         os.Getenv("RABBITMQHOST"),
+		RabbitMQPort:         rabbitmq_port,
+		RabbitMQUsername:     os.Getenv("RABBITMQUSERNAME"),
+		RabbitMQPassword:     os.Getenv("RABBITMQPASSWORD"),
+		RabbitMQVHost:        os.Getenv("RABBITMQVHOST"),
+		RabbitMQExchangeName: os.Getenv("RABBITMQEXCHANGENAME"),
+		RabbitMQQueueName:    os.Getenv("RABBITMQQUEUENAME"),
+		RabbitMQDataType:     "application/json",
+		LogPath:              os.Getenv("LOGPATH"),
+		LogLevel:             os.Getenv("LOGLEVEL"),
+		TZ:                   loc,
+	}
+
+	return cfg
+}
+
+func (c *Cfg) Initiate(logname string) *Setup {
+
+	l := helper.MakeLogger(c.LogPath+"/"+logname, true, c.LogLevel)
+	l.Info(fmt.Sprintf("Config Loaded : %#v\n", c))
+
+	return &Setup{
+		Config: c,
+		Logs:   l,
+		R:      c.InitRedisJSON(l, 0),
+		Rmqp:   c.InitMessageBroker(),
+	}
+
+}
+
+func (c *Cfg) InitRedis(l *logrus.Logger, dbindex int) *redis.Client {
+
+	r := redis.NewClient(&redis.Options{
+		Addr:     c.RedisHost + ":" + strconv.Itoa(c.RedisPort),
+		Password: c.RedisPwd,
+		DB:       dbindex,
+	})
+
+	pong, errRedis := r.Ping().Result()
+
+	if errRedis == nil && pong == "PONG" {
+
+		l.Info(fmt.Sprintf("[v] conn successful established of the redis : %s\n", pong))
+		return r
+	} else {
+
+		l.Info(fmt.Sprintf("[x] An Error occured when establishing of the redis : %#v\n", errRedis))
+
+		panic(errRedis)
+	}
+
+}
+
+func (c *Cfg) InitRedisJSON(l *logrus.Logger, dbindex int) *rueidis.Storage {
+
+	port := strconv.Itoa(c.RedisPort)
+
+	r := rueidis.New(rueidis.Config{
+		InitAddress: []string{c.RedisHost + ":" + port},
+		Username:    "",
+		Password:    c.RedisPwd,
+		SelectDB:    dbindex,
+		Reset:       false,
+		TLSConfig:   nil,
+	})
+
+	pong := r.Conn().B().Ping()
+	l.Info(fmt.Sprintf("[v] Status conn successful established of the redis : %#v\n", pong))
+
+	return r
+}
+
+func (c *Cfg) InitPsql(l *logrus.Logger) *sql.DB {
+
+	db, err := sql.Open("postgres", "postgresql://"+c.PSQLUsername+":"+c.PSQLPassword+"@"+c.PSQLHost+":"+c.PSQLPort+"/"+c.PSQLDB+"?sslmode=disable")
+	if err != nil {
+
+		// panic the function then hard exit
+		l.Info(fmt.Sprintf("[x] An Error occured when establishing of the database : %#v\n", err))
+
+		panic(err)
+
+	} else {
+
+		l.Info("[v] Database successful established\n")
+	}
+
+	return db
+}
+
+func (c *Cfg) InitMessageBroker() rmqp.AMQP {
+	var rb rmqp.AMQP
+
+	rb.SetAmqpURL(c.RabbitMQHost, c.RabbitMQPort, c.RabbitMQUsername, c.RabbitMQPassword, c.RabbitMQVHost)
+
+	rb.SetUpConnectionAmqp()
+
+	rb.SetUpChannel("direct", true, c.RabbitMQExchangeName, true, c.RabbitMQQueueName)
+	return rb
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/infraLinkit/mediaplatform-datasource
 go 1.22.2
 
 require (
+	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/gofiber/fiber/v2 v2.52.5
 	github.com/gofiber/storage/rueidis v1.2.3
 	github.com/lib/pq v1.10.9
@@ -19,6 +20,7 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/streadway/amqp v1.1.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect

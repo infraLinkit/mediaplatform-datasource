609271bff8d766dfb1353feea368153204147c9b
Wilie Wahyu Hidayat <wilie.hidayat87@gmail.com>
2024-12-10 16:45:16 +0700

    handle redis json & create campaign db

--- model/campaign_detail.go
+++ model/campaign_detail.go
@@ -10,11 +10,227 @@ import (
 )
 
 const (
-	GETCAMPAIGNDETAIL   = "SELECT id, urlservicekey, is_active, counter_mo_capping, mo_capping, status_capping, counter_mo_ratio, ratio_send, ratio_receive, status_ratio, api_url, pubid, cost, po FROM campaign_detail WHERE id = %d;"
-	COUNTERCAPPING      = "UPDATE campaign_detail SET counter_mo_capping = counter_mo_capping+1, last_update_capping = CASE WHEN counter_mo_capping >= mo_capping THEN '%s'::timestamp(0) END WHERE id = %d;"
-	COUNTERRATIO        = "UPDATE campaign_detail SET counter_mo_ratio = counter_mo_ratio+1 WHERE id = %d;"
-	UPDATESTATUSCOUNTER = "UPDATE campaign_detail SET counter_mo_capping = %d, status_capping = %t, counter_mo_ratio = %d, status_ratio = %t, last_update = '%s'::timestamp(0), last_update_capping = CASE WHEN counter_mo_capping+1 >= mo_capping THEN '%s'::timestamp(0) END WHERE id = %d"
+	NEWCAMPAIGN          = "INSERT INTO campaign (id, campaign_id, name, campaign_objective, country, advertiser) VALUES (DEFAULT, '%s', '%s', '%s', '%s', '%s')"
+	NEWCAMPAIGNDETAIL    = "INSERT INTO campaign_detail (id, urlservicekey, campaign_id, country, operator, partner, aggregator, adnet, service, keyword, subkeyword, is_billable, plan, po, cost, pubid, short_code, device_type, os, url_type, click_type, click_delay, client_type, traffic_source, unique_click, url_banner, url_landing, url_warp_landing, url_service, url_tfc_or_smartlink, glob_post, url_globpost, custom_integration, ip_address, is_active, mo_capping, counter_mo_capping, status_capping, kpi_upper_limit_capping, is_machine_learning_capping, ratio_send, ratio_receive, counter_mo_ratio, status_ratio, kpi_upper_limit_ratio_send, kpi_upper_limit_ratio_receive, is_machine_learning_auto, api_url, last_update, last_update_capping) VALUES (DEFAULT, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', %t, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', %d, %d, '%s', %t, %t, '%s', '%s', '%s', '%s', '%s', %t, '%s', '%s', '%s', %t, %d, %d, %t, %d, %t, %d, %d, %d, %t, %d, %d, %t, '%s', '%s', '%s')"
+	UPDATECAMPAIGN       = "UPDATE campaign SET name = '%s', campaign_objective = '%s', country = '%s', advertiser = '%s' WHERE campaign_id = '%s'"
+	UPDATECAMPAIGNDETAIL = "UPDATE campaign_detail SET campaign_id = '%s', country = '%s', operator = '%s', partner = '%s', aggregator = '%s', adnet = '%s', service = '%s', keyword = '%s', subkeyword = '%s', is_billable = %t, plan = '%s', po = '%s', cost = '%s', pubid = '%s', short_code = '%s', device_type = '%s', os = '%s', url_type = '%s', click_type = %d, click_delay = %d, client_type = '%s', traffic_source = %t, unique_click = %t, url_banner = '%s', url_landing = '%s', url_warp_landing = '%s', url_service = '%s', url_tfc_or_smartlink = '%s', glob_post = %t, url_globpost = '%s', custom_integration = '%s', ip_address = '%s', is_active = %t, mo_capping = %d, counter_mo_capping = %d, status_capping = %t, kpi_upper_limit_capping = %d, is_machine_learning_capping = %t, ratio_send = %d, ratio_receive = %d, counter_mo_ratio = %d, status_ratio = %t, kpi_upper_limit_ratio_send = %d, kpi_upper_limit_ratio_receive = %d, is_machine_learning_auto = %t, api_url = '%s', last_update = '%s', last_update_capping = '%s' WHERE id = %d"
+	DELCAMPAIGN          = "DELETE FROM campaign WHERE campaign_id = '%s'"
+	DELCAMPAIGNDETAIL    = "DELETE FROM campaign_detail WHERE id = %d"
+	GETCAMPAIGNDETAIL    = "SELECT id, urlservicekey, is_active, counter_mo_capping, mo_capping, status_capping, counter_mo_ratio, ratio_send, ratio_receive, status_ratio, api_url, pubid, cost, po FROM campaign_detail WHERE id = %d;"
+	COUNTERCAPPING       = "UPDATE campaign_detail SET counter_mo_capping = counter_mo_capping+1, last_update_capping = CASE WHEN counter_mo_capping >= mo_capping THEN '%s'::timestamp(0) END WHERE id = %d;"
+	COUNTERRATIO         = "UPDATE campaign_detail SET counter_mo_ratio = counter_mo_ratio+1 WHERE id = %d;"
+	UPDATESTATUSCOUNTER  = "UPDATE campaign_detail SET counter_mo_capping = %d, status_capping = %t, counter_mo_ratio = %d, status_ratio = %t, last_update = '%s'::timestamp(0), last_update_capping = CASE WHEN counter_mo_capping+1 >= mo_capping THEN '%s'::timestamp(0) END WHERE id = %d"
 )
+
+func (r *BaseModel) NewCampaign(o entity.DataCampaignAction) error {
+
+	SQL := fmt.Sprintf(NEWCAMPAIGN, o.CampaignId, o.CampaignName, o.Objective, o.Country, o.Advertiser)
+
+	stmt, err := r.DBPostgre.PrepareContext(context.Background(), SQL)
+
+	if err != nil {
+
+		r.Logs.Debug(fmt.Sprintf("NewCampaign (%s) Error %s when preparing SQL statement", SQL, err))
+
+		return err
+	}
+	defer stmt.Close()
+
+	res, err := stmt.ExecContext(context.Background())
+
+	if err != nil {
+
+		r.Logs.Debug(fmt.Sprintf("NewCampaign, SQL : %s, Error %s when update to table", SQL, err))
+
+		return err
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+
+		r.Logs.Debug(fmt.Sprintf("NewCampaign, SQL : %s, Error %s when finding rows affected", SQL, err))
+
+		return err
+	}
+
+	r.Logs.Debug(fmt.Sprintf("NewCampaign, SQL : %s, row affected : %d", SQL, rows))
+	return nil
+}
+
+func (r *BaseModel) NewCampaignDetail(o entity.DataConfig) error {
+
+	SQL := fmt.Sprintf(NEWCAMPAIGNDETAIL, o.URLServiceKey, o.CampaignId, o.Country, o.Operator, o.Partner, o.Aggregator, o.Adnet, o.Service, o.Keyword, o.SubKeyword, o.IsBillable, o.Plan, o.PO, o.Cost, o.PubId, o.ShortCode, o.DeviceType, o.OS, o.URLType, o.ClickType, o.ClickDelay, o.ClientType, o.TrafficSource, o.UniqueClick, o.URLBanner, o.URLLanding, o.URLWarpLanding, o.URLService, o.URLTFCSmartlink, o.GlobPost, o.URLGlobPost, o.CustomIntegration, o.IPAddress, o.IsActive, o.MOCapping, o.CounterMOCapping, o.StatusCapping, o.KPIUpperLimitCapping, o.IsMachineLearningCapping, o.RatioSend, o.RatioReceive, o.CounterMORatio, o.StatusRatio, o.KPIUpperLimitRatioSend, o.KPIUpperLimitRatioReceive, o.IsMachineLearningRatio, o.APIURL, o.LastUpdate, o.LastUpdateCapping)
+
+	stmt, err := r.DBPostgre.PrepareContext(context.Background(), SQL)
+
+	if err != nil {
+
+		r.Logs.Debug(fmt.Sprintf("NewCampaignDetail (%s) Error %s when preparing SQL statement", SQL, err))
+
+		return err
+	}
+	defer stmt.Close()
+
+	res, err := stmt.ExecContext(context.Background())
+
+	if err != nil {
+
+		r.Logs.Debug(fmt.Sprintf("NewCampaignDetail, SQL : %s, Error %s when update to table", SQL, err))
+
+		return err
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+
+		r.Logs.Debug(fmt.Sprintf("NewCampaignDetail, SQL : %s, Error %s when finding rows affected", SQL, err))
+
+		return err
+	}
+
+	r.Logs.Debug(fmt.Sprintf("NewCampaignDetail, SQL : %s, row affected : %d", SQL, rows))
+	return nil
+}
+
+func (r *BaseModel) UpdateCampaign(o entity.DataCampaignAction) error {
+
+	SQL := fmt.Sprintf(UPDATECAMPAIGN, o.CampaignName, o.Objective, o.Country, o.Advertiser, o.CampaignId)
+
+	stmt, err := r.DBPostgre.PrepareContext(context.Background(), SQL)
+
+	if err != nil {
+
+		r.Logs.Debug(fmt.Sprintf("UpdateCampaign (%s) Error %s when preparing SQL statement", SQL, err))
+
+		return err
+	}
+	defer stmt.Close()
+
+	res, err := stmt.ExecContext(context.Background())
+
+	if err != nil {
+
+		r.Logs.Debug(fmt.Sprintf("UpdateCampaign, SQL : %s, Error %s when update to table", SQL, err))
+
+		return err
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+
+		r.Logs.Debug(fmt.Sprintf("UpdateCampaign, SQL : %s, Error %s when finding rows affected", SQL, err))
+
+		return err
+	}
+
+	r.Logs.Debug(fmt.Sprintf("UpdateCampaign, SQL : %s, row affected : %d", SQL, rows))
+	return nil
+}
+
+func (r *BaseModel) UpdateCampaignDetail(o entity.DataConfig) error {
+
+	SQL := fmt.Sprintf(UPDATECAMPAIGNDETAIL, o.CampaignId, o.Country, o.Operator, o.Partner, o.Aggregator, o.Adnet, o.Service, o.Keyword, o.SubKeyword, o.IsBillable, o.Plan, o.PO, o.Cost, o.PubId, o.ShortCode, o.DeviceType, o.OS, o.URLType, o.ClickType, o.ClickDelay, o.ClientType, o.TrafficSource, o.UniqueClick, o.URLBanner, o.URLLanding, o.URLWarpLanding, o.URLService, o.URLTFCSmartlink, o.GlobPost, o.URLGlobPost, o.CustomIntegration, o.IPAddress, o.IsActive, o.MOCapping, o.CounterMOCapping, o.StatusCapping, o.KPIUpperLimitCapping, o.IsMachineLearningCapping, o.RatioSend, o.RatioReceive, o.CounterMORatio, o.StatusRatio, o.KPIUpperLimitRatioSend, o.KPIUpperLimitRatioReceive, o.IsMachineLearningRatio, o.APIURL, o.LastUpdate, o.LastUpdateCapping, o.Id)
+
+	stmt, err := r.DBPostgre.PrepareContext(context.Background(), SQL)
+
+	if err != nil {
+
+		r.Logs.Debug(fmt.Sprintf("NewCampaignDetail (%s) Error %s when preparing SQL statement", SQL, err))
+
+		return err
+	}
+	defer stmt.Close()
+
+	res, err := stmt.ExecContext(context.Background())
+
+	if err != nil {
+
+		r.Logs.Debug(fmt.Sprintf("NewCampaignDetail, SQL : %s, Error %s when update to table", SQL, err))
+
+		return err
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+
+		r.Logs.Debug(fmt.Sprintf("NewCampaignDetail, SQL : %s, Error %s when finding rows affected", SQL, err))
+
+		return err
+	}
+
+	r.Logs.Debug(fmt.Sprintf("NewCampaignDetail, SQL : %s, row affected : %d", SQL, rows))
+	return nil
+}
+
+func (r *BaseModel) DelCampaign(o entity.DataCampaignAction) error {
+
+	SQL := fmt.Sprintf(DELCAMPAIGN, o.CampaignId)
+
+	stmt, err := r.DBPostgre.PrepareContext(context.Background(), SQL)
+
+	if err != nil {
+
+		r.Logs.Debug(fmt.Sprintf("DelCampaign (%s) Error %s when preparing SQL statement", SQL, err))
+
+		return err
+	}
+	defer stmt.Close()
+
+	res, err := stmt.ExecContext(context.Background())
+
+	if err != nil {
+
+		r.Logs.Debug(fmt.Sprintf("DelCampaign, SQL : %s, Error %s when update to table", SQL, err))
+
+		return err
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+
+		r.Logs.Debug(fmt.Sprintf("DelCampaign, SQL : %s, Error %s when finding rows affected", SQL, err))
+
+		return err
+	}
+
+	r.Logs.Debug(fmt.Sprintf("DelCampaign, SQL : %s, row affected : %d", SQL, rows))
+	return nil
+}
+
+func (r *BaseModel) DelCampaignDetail(o entity.DataConfig) error {
+
+	SQL := fmt.Sprintf(DELCAMPAIGNDETAIL, o.Id)
+
+	stmt, err := r.DBPostgre.PrepareContext(context.Background(), SQL)
+
+	if err != nil {
+
+		r.Logs.Debug(fmt.Sprintf("DelCampaignDetail (%s) Error %s when preparing SQL statement", SQL, err))
+
+		return err
+	}
+	defer stmt.Close()
+
+	res, err := stmt.ExecContext(context.Background())
+
+	if err != nil {
+
+		r.Logs.Debug(fmt.Sprintf("DelCampaignDetail, SQL : %s, Error %s when update to table", SQL, err))
+
+		return err
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+
+		r.Logs.Debug(fmt.Sprintf("DelCampaignDetail, SQL : %s, Error %s when finding rows affected", SQL, err))
+
+		return err
+	}
+
+	r.Logs.Debug(fmt.Sprintf("DelCampaignDetail, SQL : %s, row affected : %d", SQL, rows))
+	return nil
+}
 
 func (r *BaseModel) GetCampaignDetail(o entity.DataConfig) (entity.DataConfig, error) {
 
--- model/redisjson.go
+++ model/redisjson.go
@@ -100,3 +100,73 @@ func (h *BaseModel) SetCounterData(key string, path string, val string) {
 	}
 
 }
+
+func (h *BaseModel) IndexRedis(key string, field string) {
+
+	h.R.Conn().B().FtCreate().Index(key).Prefix(1)
+
+	//`$.campaign_id AS campaign_id TEXT $.pixel AS pixel TEXT $.user_agent AS user_agent TEXT $.os AS os TEXT $.browser AS browser TEXT $.ips AS ips TEXT $.user_is_rejected AS user_is_rejected TAG $.user_is_duplicated AS user_is_duplicated TAG $.refferal_url AS refferal_url TEXT $.handset_code AS handset_code TEXT $.handset_type AS handset_type TEXT $.pixel_is_used AS pixel_is_used TAG`
+
+	//Create Indexing key
+	result := h.R.Conn().Do(context.Background(), h.R.Conn().B().FtCreate().Index(key).OnJson().Schema().FieldName(field).Text().Build())
+	isIdxCreated, err := result.AsBool()
+
+	if err != nil {
+		h.Logs.Info(fmt.Sprintf("[v] Created FT idx key ( %s ) : %t, %#v...\n\r", key, isIdxCreated, result))
+	} else {
+
+		if !isIdxCreated {
+			h.Logs.Info(fmt.Sprintf("[x] Failed created FT idx key ( %s ) : %#v...\n\r", key, isIdxCreated))
+		} else {
+			h.Logs.Info(fmt.Sprintf("[v] Success created FT idx key ( %s ) : %t, %#v...\n\r", key, isIdxCreated, result))
+		}
+
+	}
+
+}
+
+func (h *BaseModel) SetData(key string, path string, val string) {
+
+	// Get Config Data Landing
+	ctx := context.Background()
+
+	if err := h.R.Conn().Do(ctx, h.R.Conn().B().JsonSet().Key(key).Path(path).Value(val).Build()).Error(); err != nil {
+		h.Logs.Debug(fmt.Sprintf("Set data error key (%s), path (%s), err : %#v ...\n", key, path, err))
+	} else {
+		h.Logs.Debug(fmt.Sprintf("Set data success key (%s), path (%s) ...\n", key, path))
+	}
+
+}
+
+func (h *BaseModel) SetExpireData(key string) {
+
+	//Set key expire and delete automatically
+	result := h.R.Conn().Do(context.Background(), h.R.Conn().B().Expire().Key(key).Seconds(h.Config.RedisKeyExpiration).Build())
+
+	isExpiredCreated, err := result.AsBool()
+
+	if err != nil {
+		h.Logs.Info(fmt.Sprintf("[x] Error Set duration expire key ( %s ) : %t, %#v...\n\r", key, isExpiredCreated, result))
+	} else {
+
+		if !isExpiredCreated {
+			h.Logs.Info(fmt.Sprintf("[x] Failed Set duration expire key ( %s ) : %#v...\n\r", key, isExpiredCreated))
+		} else {
+			h.Logs.Info(fmt.Sprintf("[v] Success Set duration expire key ( %s ) : %t, %#v...\n\r", key, isExpiredCreated, result))
+		}
+
+	}
+}
+
+func (h *BaseModel) DelData(key string, path string) {
+
+	// Get Config Data Landing
+	ctx := context.Background()
+
+	if err := h.R.Conn().Do(ctx, h.R.Conn().B().JsonDel().Key(key).Path(path).Build()).Error(); err != nil {
+		h.Logs.Debug(fmt.Sprintf("Del data error key (%s), path (%s), err : %#v ...\n", key, path, err))
+	} else {
+		h.Logs.Debug(fmt.Sprintf("Del data success key (%s), path (%s) ...\n", key, path))
+	}
+
+}

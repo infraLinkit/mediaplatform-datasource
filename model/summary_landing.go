77d20f9a21985c58354df9607bb9d071a1ff00f2
Wilie Wahyu Hidayat <wilie.hidayat87@gmail.com>
2025-09-30 18:17:52 +0700

    customize query total landing

--- model/summary_landing.go
+++ model/summary_landing.go
@@ -6,13 +6,29 @@ import (
 	"github.com/infraLinkit/mediaplatform-datasource/entity"
 )
 
-func (r *BaseModel) GetURLServiceFromSummaryLanding() ([]entity.SummaryLanding, error) {
+func (r *BaseModel) GetURLServiceFromSummaryLanding(event_date string, with_limit int) ([]entity.SummaryLanding, error) {
 
 	var (
 		ss []entity.SummaryLanding
 	)
 
-	query := r.DB.Model(&entity.SummaryLanding{}).Select("url_service_key", "country", "operator", "partner", "adnet", "service", "url_service", "response_url_service_time").Where("summary_date_hour <= NOW() - INTERVAL '1 hour'")
+	query := r.DB.Model(&entity.SummaryLanding{}).Select("url_service_key", "country", "operator", "partner", "adnet", "service", "url_service", "response_url_service_time")
+
+	switch event_date {
+	case "1 HOUR AGO":
+		query.Where("summary_date_hour <= NOW() - INTERVAL '1 hour'")
+	case "1 DAY AGO":
+		query.Where("summary_date_hour <= NOW() - INTERVAL '1 days'")
+	default:
+		query.Where("DATE(summary_date_hour) = '" + event_date + "'")
+	}
+
+	if with_limit > 0 {
+		query.Limit(with_limit)
+	}
+
+	query.Order("landing DESC")
+
 	if rows, err := query.Rows(); err == nil {
 
 		defer rows.Close()
